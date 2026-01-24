8333a3e3a5be52b7e100dc2530bb90e4a12f85b1
udi-star <udi@ibizatarot.com>
2026-01-24 12:31:08 +0100

    ✨ Manifesting Deck: Butterfly Tarot  (Additive Deploy)

--- app/butterfly-tarot/service-worker.js
+++ app/butterfly-tarot/service-worker.js
@@ -1,19 +1,4 @@
-const CACHE_NAME = 'butterfly-tarot-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './css/style.css',
-  './js/app.js',
-  './manifest.json',
-  './assets/cards/back.webp',
-  './assets/cards/cover.webp',
-  './assets/cards/logo.webp'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
+const CACHE_NAME = 'arcana-butterfly-tarot-v1';
+const ASSETS = ['index.html', 'css/style.css', 'js/app.js', 'assets/cards/back.webp', 'assets/cards/cover.webp'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
